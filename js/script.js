6f06745f1dd730138d1135863249deffbfd27967
lindng <lindang1504@hotmail.com>
2021-01-23 20:25:39 +0100

    this

--- js/script.js
+++ js/script.js
@@ -33,3 +33,67 @@ console.log(paragraph);
 for (let i = 0; i < paragraph.length; i++) {
     paragraph[i].style.color = "red";
 }
+
+
+// QUESTION 6 //
+
+const resultsContainer = document.querySelector(".results");
+
+resultsContainer.innerHTML = "<p> New Paragraph </p>";
+
+resultsContainer.style.background = "yellow";
+
+
+// QUESTION 7 // 
+
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+function oneParameter (list) {
+    for (let i = 0; i < list.length; i++) {
+        console.log(list[i].name);
+    }
+}
+
+oneParameter (cats);
+
+
+// QUESTION 8 // 
+
+function createCats (cats) {
+
+    let html = "";
+
+    for (let i = 0; i < cats.length; i++) {
+        let unknownAge = "Age unknown";
+
+        if(cats[i].age) {
+            unknownAge = cats[i].age;
+        }
+        html += `<div class="listCats">
+        <h5>${cats[i].name}</h5>
+        <p>Age: ${unknownAge}</p>
+        </div>`;
+        
+    }
+    return html;
+    
+}
+
+createCats (cats);
+
+const newHTML = createCats(cats);
+
+const catsContainer = document.querySelector(".cat-container");
+catsContainer.innerHTML = newHTML;
